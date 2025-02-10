e11925dc7261ddc769e23f9b39a549ecfaa3968a
Jesse Elliott <jesseelliott@outlook.com>
2025-02-10 02:32:58 -0500

    Add quaddicted dropdown generator
    Needs a bit more work in the "no tag for startmap" area but is
    pretty functional as is

--- element.js
+++ element.js
@@ -15,17 +15,39 @@ async function fetchJsonData(sha) {
 
 function buildResultString(json) {
     const tag = json.tags.find(tag => tag.includes("zipbasedir"));
-    if (!tag) {
+    let type;
+    let gamedir;
+    if (tag) {
+         type = tag.includes("maps") ? "map" : "gamedir";
+         gamedir = tag.replace("zipbasedir=", "").replace("/maps/", "").replace("/", "");
+    } else {
         console.log("No zipbasedir tag found.");
-        return null;
+        type = "mod-folder"
+        gamedir = Object.keys(json.files)[0].split("/")[0]
     }
-
-    const type = tag.includes("maps") ? "map" : "mod";
-    const gamedir = tag.replace("zipbasedir=", "").replace("/maps/", "");
-
+    if (!type || !gamedir) throw new Error("uh oh, what do")
     return `q1package:${json.urls[0]},${type},${gamedir}`;
 }
-const json = await fetchJsonData("d8959d3f3d3dd3043b0a784884549213e8dbe3f7626a586b21964883eb3b1581");
 
+const segments = new URL(location.href).pathname.split("/");
+const last = segments.pop() || segments.pop();
+const json = await fetchJsonData(last);
+console.log(json)
 const result = buildResultString(json);
+function changeChoice(value) {
+    document.querySelector("#one-click").href = result + "," + value;
+}
+let mapChoices = [];
+let startmapTags = json.tags.filter(it => it.includes("startmap"))
+if (startmapTags.length > 0){
+    startmapTags.map(it => it.replace("startmap=", "")).forEach(it => mapChoices.push(it));
+} else {
+   mapChoices.push("start")
+}
+function createElement(){
+    var div = document.createElement("tr")
+   div.innerHTML = `<td><select ${mapChoices.length > 1 ? "" : "readonly"} onchange="changeChoice(this.value)">${mapChoices.map(choice => `<option value="${choice}">${choice}</option>`)}</select></td><td><a id="one-click" href="${result + "," + mapChoices[0]}">One-click Launch</a></td>`
+    return div
+}
+document.querySelector("#infos tbody").prepend(createElement())
 if (result) console.log(result);
